593ec33668b9ee95860889fcf5ae9f4fdde53b59
Aaahana Mehrotra <83857184+aaahanamehrotra@users.noreply.github.com>
2026-06-23 15:28:52 +0530

    Commits on Jun 23, 2026 Feat: Global Context Provider for Auth  (#5)
    * Feat: Global Context Provider for Auth
    
    Added Global Context provide which returns isLoggedIn, setLoggedIn, isGlobalLoading, setGlobalLoading, isAdmin, setAdmin. Will fetch {process.env.NEXT_PUBLIC_AUTH_URL}/api/auth/me when the backend is written
    
    * fix: Add Context Provider file
    
    * Fix: lint error in ci

---
+++ privasyncdemo/components/ContextProvider.tsx
@@ -0,0 +1,84 @@
+"use client";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+
+const ADMIN_CODE = 99;
+interface GlobalContextType {
+  isLoggedIn: boolean | null;
+  setLoggedIn: (isLoggedIn: boolean | null) => void;
+  isGlobalLoading: boolean;
+  setGlobalLoading: (isGlobalLoading: boolean) => void;
+  isAdmin: boolean;
+  setAdmin: (isAdmin: boolean) => void;
+}
+
+const GlobalContext = createContext<GlobalContextType>({
+  isLoggedIn: null,
+  setLoggedIn: () => {},
+  isGlobalLoading: false,
+  setGlobalLoading: () => {},
+  isAdmin: false,
+  setAdmin: () => {},
+});
+
+export function GlobalContextProvider({ children }: { children: ReactNode }) {
+  const [isLoggedIn, setLoggedIn] = useState<boolean | null>(null);
+  const [isGlobalLoading, setGlobalLoading] = useState<boolean>(true);
+  const [isAdmin, setAdmin] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function verifyUserStatus() {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_AUTH_URL}/api/auth/me`,
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
+        if (response.ok) {
+          setLoggedIn(true);
+          // authorization
+          const data = await response.json();
+          if(data.role == ADMIN_CODE){
+            setAdmin(true)
+          }else{
+            setAdmin(false)
+          }
+        }else {
+          setLoggedIn(false);
+        }
+    }catch {
+        setGlobalLoading(false);
+      } finally {
+        setGlobalLoading(false);
+      }
+    }
+    // Uncomment for function to be called after backend is written
+    // verifyUserStatus();
+
+    // Hardcoding until backend is written
+    // setLoggedIn(true)
+    // setAdmin(false)
+  }, []);
+
+  const value = {
+    isLoggedIn,
+    setLoggedIn,
+    isGlobalLoading,
+    setGlobalLoading,
+    isAdmin,
+    setAdmin,
+  };
+
+  return (
+    <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
+  );
+}
+
+export const useGContext = () => useContext(GlobalContext);
